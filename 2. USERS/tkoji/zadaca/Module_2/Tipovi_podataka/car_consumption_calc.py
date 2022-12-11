0ceb99129db5342e9acb435e33c60da7bfacb00d
TomTomX <TomTom@X>
2022-12-11 17:50:45 +0100

    Car consumption calcultor

---
+++ 2. USERS/tkoji/zadaca/Module_2/Tipovi_podataka/car_consumption_calc.py
@@ -0,0 +1,19 @@
+""" Kreirajte varijable (imenujte ih i dodijelite im odgovarajuću vrijednost) te ispišite na ekran odgovarajuće vrijednosti, za:
+• Ako automobil troši 5.3 litara na 100 km i ako je cijena goriva 9.56 kn po litri (nije važno kojeg goriva).
+Izračunajte koliko košta 1 km vožnje automobilom.
+Prikažite mjesečni trošak (30 dana) odlaska na posao automobilom koji je udaljen 20 km u jednom smjeru. """
+
+fuel_price = 9.53
+car_consumption = 5.3
+avg_daily_km = 40
+avg_daily_per_month = 30
+
+fuel_price_per_km = round((fuel_price * car_consumption) / 100,3)
+print(f'You car consumption per kilometer is ~{fuel_price_per_km}HRK')
+
+fuel_cost_per_month = (avg_daily_km * fuel_price_per_km ) * avg_daily_per_month 
+print(f'Total cost of fuel per month: {fuel_cost_per_month}HRK')
+
+
+
+
---
+++ 2. USERS/tkoji/zadaca/Module_2/Tipovi_podataka/car_consumption_calc_advanced.py
@@ -0,0 +1,22 @@
+#fuel_price = 9.53
+#car_consumption = 5.3
+#avg_daily_milage = 40
+#avg_days_per_month = 30
+
+car_consumption = float(input('\nEnter your average car consumption: '))
+fuel_price = float(input('\nAdd current petrol price: '))
+avg_daily_milage = float(input('\nWhat is your average daily milage? '))
+avg_days_per_month = float(input('\nHow many days a month you drive your car? '))
+
+fuel_price_per_km = round((fuel_price * car_consumption) / 100,3)
+print(f'You car consumption per kilometer is ~{fuel_price_per_km}HRK')
+
+fuel_cost_per_month = (avg_daily_milage * fuel_price_per_km ) * avg_days_per_month 
+print(f'Total cost of fuel per month: {fuel_cost_per_month}HRK')
+
+#TODO
+# - what is the car task size?
+# - based on consumption how many times a month a client will need to refill his car
+# - what will be the client montly cost if the petrol price surges for 23%
+# - what is the total car milage?
+# - how much money in total client spent driving that car?
