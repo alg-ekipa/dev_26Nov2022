395a20e5847daf2a3d46e31e1e2378a67192183a
TomTomX <TomTom@X>
2022-12-12 08:21:16 +0100

    Tom: Car consumption calc improvments

--- 2. USERS/tkoji/zadaca/Module_2/Tipovi_podataka/car_consumption_calc_advanced.py
+++ 2. USERS/tkoji/zadaca/Module_2/Tipovi_podataka/car_consumption_calc_advanced.py
@@ -1,22 +1,26 @@
-#fuel_price = 9.53
-#car_consumption = 5.3
-#avg_daily_milage = 40
-#avg_days_per_month = 30
-
+""" User inputs """
 car_consumption = float(input('\nEnter your average car consumption: '))
-fuel_price = float(input('\nAdd current petrol price: '))
-avg_daily_milage = float(input('\nWhat is your average daily milage? '))
-avg_days_per_month = float(input('\nHow many days a month you drive your car? '))
+fuel_price = float(input('Add current petrol price: '))
+avg_daily_milage = float(input('What is your average daily milage? '))
+avg_days_per_month = float(input('How many days a month you drive your car? '))
+print("\nPlase give us more information so we calculate more infromation for you.\n")
+car_tank_size = float(input('What is the car tank size? '))
+car_total_milage = float(input('What is the total car milage? '))
+print("\nThank you! Here are you statistics.\n")
 
+""" Calculated fields """
 fuel_price_per_km = round((fuel_price * car_consumption) / 100,3)
-print(f'You car consumption per kilometer is ~{fuel_price_per_km}HRK')
+fuel_cost_per_month = round((avg_daily_milage * fuel_price_per_km ) * avg_days_per_month,2)
+fuel_total_liters_per_month = fuel_cost_per_month / fuel_price
+fuel_refill_times = fuel_total_liters_per_month //  fuel_total_liters_per_month
+#TODO - wrong calculation
+#fuel_price_surge = round(((23 / fuel_price)* 100 ) * avg_daily_milage * avg_days_per_month,2)
+fuel_total_money_spent = round(car_total_milage * fuel_price_per_km,2)
 
-fuel_cost_per_month = (avg_daily_milage * fuel_price_per_km ) * avg_days_per_month 
+""" User outputs """
 print(f'Total cost of fuel per month: {fuel_cost_per_month}HRK')
-
-#TODO
-# - what is the car tank size?
-# - based on consumption how many times a month a client will need to refill his car
-# - what will be the client montly cost if the petrol price surges for 23%
-# - what is the total car milage?
-# - how much money in total client spent driving that car?
+print(f'You car consumption per kilometer is ~{fuel_price_per_km}HRK')
+print(f'With an average consumption of {car_consumption} you will need to refill you car {fuel_refill_times} per month.')
+#TODO - wrong calculation
+# print(f'If fuel price surges for 23% your montly expense for the fuel will be {fuel_price_surge}HRK')
+print(f'For the total milage of \'{car_total_milage}\' you spent {fuel_total_money_spent}HRK.')
