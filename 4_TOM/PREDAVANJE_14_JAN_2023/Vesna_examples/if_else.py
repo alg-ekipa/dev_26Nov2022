6cdbe13fc0538c617c5e53985c14428d3d095cb8
Tom <tom.tom.x>
2023-01-14 11:25:32 +0100

    Tom: lectures 14th Jan 2023 part 1

---
+++ 4_TOM/PREDAVANJE_14_JAN_2023/Vesna_examples/if_else.py
@@ -0,0 +1,10 @@
+
+b = 19
+
+if b < 15:
+    print("Number", b, "is lower thab 15.")
+    print("We are in the if block now.")
+else:
+    print("Number", b, "is greater than 15.")
+    print("We are in the else block now.")
+    
