bb59ed587761d775ebf26966173f62735950457a
Tom Kojic <tomislav.kojic from Hogwards>
2023-01-14 22:15:09 +0100

    Tom: Palindrome intentional error

--- 4_TOM/PREDAVANJE_14_JAN_2023/Vesna_examples/loop_control/for/_excercise_palindrome.py
+++ 4_TOM/PREDAVANJE_14_JAN_2023/Vesna_examples/loop_control/for/_excercise_palindrome.py
@@ -4,8 +4,7 @@
 # TODO - this program does not handle the sentences ie. whitespaces.
 word = input("Check if work is palindrome: ")
 word = word.strip()
-word_reversed = word[::-1]
-
+word_reversed = word[:::]    
 if word_reversed == word:
     print("The word is plaindrome!")
 else: 
