77ef97418fccca0992a9df2ba090001f77bc26fd
Tom <tom.tom.x>
2023-01-21 11:03:10 +0100

    Tom: Exit handling

--- 4_TOM/homework_ready/palindrome_edi.py
+++ 4_TOM/homework_ready/palindrome_edi.py
@@ -1,14 +1,11 @@
-# REMEMBER - a string is already a list! 
-# word = input("Check if work is palindrome: ").lower()
-# # THE NEXT LINE REMOVES WHITESPACE IF ANY
-# word = word.replace(" ",'')
-# print(word)
-# word_reversed = word[::-1]
-
 while True: 
     word = input("Check if work is palindrome or enter to quit:\n ").lower()
     word = word.replace(" ",'')
-    word_reversed = word[::-1]
+    print(word)
+    word = list(word)
+    print(word)
+    word_reversed = word.reverse()
+    print(word_reversed)
     if not word:
         break
     if word_reversed == word:
