2b6ddc2aba954f2ec2420e656bb63b1f026da232
SaMr <sa@san.com>
2023-01-21 11:58:25 +0100

    San

--- 2_USERS/smsalo/private/Module_2/Kontrola toka/konverzija.py
+++ 2_USERS/smsalo/private/Module_2/Kontrola toka/konverzija.py
@@ -8,22 +8,22 @@ while True:
         if smjer=='a':
             km=float(input('Unesite kilometre: '))
             milja=km*0.6214
-            print(f'{km} kilometara iznosi {milja} milja.')
+            print(f'{km} kilometara iznosi {round(milja,2)} milja.')
         elif smjer=='b':
             milja=float(input('Unesite milje: '))
             km=milja/0.6214
-            print(f'{milja} milja iznosi {km} kilometara.')
+            print(f'{milja} milja iznosi {round(km,2)} kilometara.')
     
     if konv==2:
         smjer=input('Odabrali ste konverziju temperature. Odaberite smjer konverzije: \n a - iz °C u °F \n b - °F u °C \n ')
         if smjer=='a':
             temp_C=int(input('Unesite temperaturu u °C: '))
             temp_F=temp_C*(9/5)+32
-            print(f'{temp_C} °C iznosi {temp_F} °F.')
+            print(f'{temp_C} °C iznosi {round(temp_F,2)} °F.')
         elif smjer=='b':
             temp_F=int(input('Unesite temperaturu u °F: '))
             temp_C=(temp_F-32)/9/5
-            print(f'{milja} milja iznosi {km} kilometara.')
+            print(f'{temp_F} °F iznosi {round(temp_C,2)} °C.')
 
     odluka=input('Zelite li nastaviti unositi? Za DA unesite 1, za NE unesite 0. ')
     if odluka=='0':
