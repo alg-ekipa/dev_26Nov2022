9656cc530445bc999cd29ec7b61e571de544b4aa
Ivan <ivanhreodm.com>
2023-01-21 09:15:47 +0100

    Gelo: sve promjene

---
+++ 2_USERS/igel/private/Module_2/varijable_tipovi_podataka/ipkonverzija.py
@@ -0,0 +1,17 @@
+
+'''
+172.27.240.1 ip mog PC-ja
+'''
+
+prvi_oktet = bin(172).replace('0b','')
+prvi_oktet_final = prvi_oktet.replace('0b','')
+drugi_oktet =bin(27)
+treci_oktet =bin(240)
+cetvrti_oktet = bin(1)
+
+
+print(prvi_oktet)
+print(type(prvi_oktet))
+
+
+
