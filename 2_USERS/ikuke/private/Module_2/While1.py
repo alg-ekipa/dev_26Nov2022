f1bad4e1e84a4390da6613c5cfb7b5ec5a7e622a
Your Name <ajgor@ajgor.com>
2023-01-21 10:06:34 +0100

    #proba

---
+++ 2_USERS/ikuke/private/Module_2/While1.py
@@ -0,0 +1,14 @@
+brojevi = []
+for i in range(1,21):
+    brojevi.append(i)
+
+for broj in brojevi:
+    print(broj, end = " ")
+
+print()
+print(brojevi)
+
+i = 0
+while i < 20:
+    print(brojevi[i])
+    i +=1
