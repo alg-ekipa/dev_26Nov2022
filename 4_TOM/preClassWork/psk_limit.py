629ef540e7b32dc514831c7a9051aa3a1da31685
Tom Kojic <tomislav.kojic from Hogwards>
2023-01-21 17:58:50 +0100

    Tom: PSK game with limit completed

--- 4_TOM/preClassWork/psk_limit.py
+++ 4_TOM/preClassWork/psk_limit.py
@@ -5,17 +5,19 @@ CEND = '\033[0m'
 CYEL = '\33[43m'
 CEND =  '\033[0m'
 
-#game = input("Do you want to play? ")
+game = input("Do you want to play? ")
 game =  'y'
 count_user = 0
 count_computer = 0
+count_tie = 0 
 
 while game == 'y':
+    rounds = int(input("How many rounds you want to play? "))
     game_count = 0
-    while game_count < 5: 
+    while game_count < rounds: 
         choice_computer = random.randrange(1,4)
         print("Computer choose", choice_computer,'\n')
-        choice_user = str(input('''Let's play. Input the number or 'q' to exit the game.
+        choice_user = str(input('''Let's play! Input the number or 'q' to exit the game.
             1) Rock
             2) Scissiors
             3) Paper\n
@@ -25,38 +27,45 @@ while game == 'y':
         else:
             choice_user = str(choice_user).lower()
         if choice_user == choice_computer:
-            print(CYEL + "Tie, not of the users gets the point" + CEND)
+            print(CYEL + "Tie, non of the users gets the point." + CEND)
+            count_tie += 1 
+            game_count += 1
             
         elif choice_user == 1 and choice_computer == 2:
             print(CYEL + "User won." + CEND)
             count_user += 1
-        
+            game_count += 1
+            
         elif choice_user == 1 and choice_computer == 3:
             print(CYEL + "Computer won." + CEND)
             count_computer += 1
-            
+            game_count += 1
+                     
         elif choice_user == 2 and choice_computer == 1:
             print(CYEL +"Computer won." + CEND)
             count_computer += 1
-        
+            game_count += 1
+                  
         elif choice_user == 2 and choice_computer == 3:
             print(CYEL +"Computer won." + CEND)
             count_computer += 1      
+            game_count += 1
             
         elif choice_user == 3 and choice_computer == 1:
             print(CYEL +"User won." + CEND)
             count_user += 1
-        
+            game_count += 1
+               
         elif choice_user == 3 and choice_computer == 2:
             print(CYEL +"Computer won." + CEND)
             count_computer += 1
-        
+            game_count += 1
+               
         elif choice_user == 'q':
             print("\nThanks for playing. The result is below.\n")
             break
         else:
-            print(CRED, "Invalid input, please choose the options from the menu or type 'q' to quit.", CEND)
+            print(CRED + "Invalid input, please choose the options from the menu or type 'q' to quit." + CEND)
     break
-print("End of the game. Here are the statistics for the 5 rounds.")
-print(f'User: {count_user}\nComputer: {count_computer}')
-1
+print(f"\nEnd of the game. Here are the game statistics played in {rounds} round(s).")
+print(f'USER: {count_user}\nCPU: {count_computer}\nTIE: {count_tie}')
