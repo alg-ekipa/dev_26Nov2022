033a156d903208a7cf754be996480319a66fe0d5
Your Name <ajgor@ajgor.com>
2023-02-04 13:26:25 +0100

    #unos

---
+++ 2_USERS/ikuke/private/Module_2/4.2.2023/akordi.py
@@ -0,0 +1,44 @@
+#tonovi s while petljom za unos
+tonovi=['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'H']
+
+
+
+def funkcija():
+    #print(pocetni)
+    broj=tonovi.index(pocetni)
+                                            
+                                            
+    #print(broj)
+    broj_3=broj+2
+    broj_4=broj+3
+    broj_7=broj+6
+    #print(broj_3, broj_4, broj_7)
+
+    ton_3=tonovi[broj_3]
+    ton_4=tonovi[broj_4]
+    ton_7=tonovi[broj_7]
+
+    print(f'Za početni ton', pocetni, 'ljestvica', pocetni, 'dur se sastoji od tonova:', pocetni, ton_4, ton_7)
+    print(f'Za početni ton', pocetni, 'ljestvica', pocetni_mol, 'mol se sastoji od tonova:', pocetni, ton_3, ton_7)
+
+
+#printanje(tonovi)
+
+
+while True:
+    ton_POCETNI=input('Unesi početni ton ')
+    
+    while ton_POCETNI.upper() not in tonovi:
+        ton_POCETNI=input('Unijeli ste krivo! Unesite pocetni ton: ')
+    else:
+        
+
+        tonovi.extend(tonovi)
+        pocetni=ton_POCETNI.upper()
+      
+        pocetni_mol=ton_POCETNI.lower()
+        funkcija()
+
+    izbor=input('Zelite li nastaviti unositi akorde? Za DA unesite 1, za NE unesite 0. ')
+    if izbor=='0':
+        break
