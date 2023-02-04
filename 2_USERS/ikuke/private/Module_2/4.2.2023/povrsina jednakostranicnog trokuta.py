b9ed4122cb2444258f5dd72979595b4edc09a3ce
Your Name <ajgor@ajgor.com>
2023-02-04 10:15:24 +0100

    #proba

--- 2_USERS/ikuke/private/Module_2/4.2.2023/povrsina jednakostranicnog trokuta.py
+++ 2_USERS/ikuke/private/Module_2/4.2.2023/povrsina jednakostranicnog trokuta.py
@@ -11,6 +11,13 @@ def racunaj_visinu_trokuta(a):
     #return m.pow(r,2)*m.pi
     return a*sqrt(3)/2
 
+
+
+
+
+
+
+
 a=input('upisi duljinu stranice jednakostranicnog trokuta: ')
 
 a=int(a)
@@ -20,3 +27,4 @@ P= racunaj_povrsinu_trokuta(a)
 V= racunaj_visinu_trokuta(a)
 
 print (f'Povrsina jednakostranicnog trokuta duljine stranice {a} je {P}, a visina na stranicu {a} je {V}')
+print()
