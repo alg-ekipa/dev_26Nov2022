1f5b14b6555d9160510472a13a6933f11b4051e2
Your Name <ajgor@ajgor.com>
2023-02-11 12:00:37 +0100

    #proba

--- 2_USERS/ikuke/private/Module_2/PRIJAVA.py
+++ 2_USERS/ikuke/private/Module_2/PRIJAVA.py
@@ -10,14 +10,30 @@ def pregled_korisnici():
         print(f' Ime: {v[0]}, Prezime: {v[1]}')
 
 
-
-
-
 def tko_je_admin():
     """ Funkcija koja ispisuje ime i prezime administratora """
     for k,v in korisnici.items():
         if (k== 'admin'):
             print(f' Ime: {v[0]}, Prezime: {v[1]} -------------------------- je Admin')
+
+
+def dodavanje_novog_korisnika_u_listu():
+        #punjenje rječnika iz dvije liste - jedna su klučevi, a druga su vrijednosti
+
+    rjecnik_iz_liste = {}
+
+    lista_kljuceva = [1, 2, 3, 4, 5]
+    lista_vrijednosti = ['python', 'c++', 'java', 'perl', 'asp.net']
+
+    for i in range (len(lista_kljuceva)):
+        kljuc=lista_kljuceva[i]
+        vrijednost=lista_vrijednosti[i]
+        rjecnik_iz_liste[kljuc]=vrijednost
+
+
+    for k, v in rjecnik_iz_liste.items():
+        print(f'{k} : {v}')
+
 
 
 
@@ -27,12 +43,29 @@ def dohvati_admin_pswd():
             return v[2]
 
 
+def dodavanje_korisnika():
+    odg = input ('Želite li se dodati korisnika? da/ne ')
+    if odg == 'da':
+        username_korisnika=input('Unesi korisničko ime:')
+        ime_korisnika=input('Unesi ime kosrinika:')
+        prezime_korisnika=input('Unesi prezime kosrinika:')
+        sifra_korisnika=input('Unesi šifru korisnika:')
+        print(f'korisnik {ime_korisnika} {prezime_korisnika} ima username {username_korisnika} i šifru {sifra_korisnika}')
+        korisnici.update({username_korisnika:['ime_korisnika','prezime_korisnika','sifra_korisnika']})
+        
+        
+    else:
+        print('unijeli ste da ne želite dodati novog korinika')
+
+
 
 def logiranje(uneseni_username):
     if uneseni_username in korisnici.keys():
         uneseni_password= input('unesi lozinku: ')
         if uneseni_password == korisnici[uneseni_username][2]:
+            print('___________________________________________________________________________________')
             print(f'Dobrodosli, {korisnici[uneseni_username][0] } {korisnici[uneseni_username][1]}')
+            print('___________________________________________________________________________________')
             
     else:
         print(f'Nepostojeće korisničko ime')
@@ -42,6 +75,16 @@ tko_je_admin()
 a=dohvati_admin_pswd()
 print('Admin password je ', a)
 
+dodavanje_korisnika()
 
-username=input('Unesi korisničko ime:')
-logiranje(username)
+while 1:
+    odg = input ('Želite li se prijaviti? da/ne ')
+    if odg== 'da':
+
+        username=input('Unesi korisničko ime:')
+        logiranje(username)
+        
+    else:
+        break
+
+pregled_korisnici()
