08ad439ceb5d9ed55f52390a0f7875ca24290bf6
Your Name <ajgor@ajgor.com>
2023-02-11 09:52:51 +0100

    #

--- 2_USERS/ikuke/private/Module_2/KARTICA.py
+++ 2_USERS/ikuke/private/Module_2/KARTICA.py
@@ -1,9 +1,13 @@
 broj_kartice=input('unesi broj kartice ')
 zakodirani_broj_kartice=''
 
-rijec= 'programiranje'
+print ('unesite znak za maskiranje broja kartice:')
+znak=input()
 duljina=len(broj_kartice)
 i=0
+
+
+
 
 for slovo in broj_kartice:
 
@@ -11,8 +15,10 @@ for slovo in broj_kartice:
     i=i+1
     if i>(duljina-4): 
         zakodirani_broj_kartice=zakodirani_broj_kartice+slovo
+    elif slovo=='-':
+        zakodirani_broj_kartice=zakodirani_broj_kartice+slovo
     else:
-        zakodirani_broj_kartice=zakodirani_broj_kartice+'*'
+        zakodirani_broj_kartice=zakodirani_broj_kartice+znak
 
 
 
