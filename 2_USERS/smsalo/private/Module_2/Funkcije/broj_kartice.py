817850643080ada885f8119b9b592656407a1741
SaMr <sa@san.com>
2023-02-11 09:56:13 +0100

    San

---
+++ 2_USERS/smsalo/private/Module_2/Funkcije/broj_kartice.py
@@ -0,0 +1,33 @@
+# program koji će za uneseni broj kartice sakriti sve znamenke osim zadnje 4
+
+    
+while True:
+    br_kartice=input('Unesite br. kartice: ')
+    if 5<len(br_kartice)<=16:
+        znak=input('Unesite znak s kojim želite zaštititi broj kartice: ')
+        break
+    else:
+        print('Broj nije u zadanom rasponu, pokušajte ponovo! ')
+print()
+
+
+brojevi=list(br_kartice)
+print(brojevi)
+
+skrivena=[]
+for i in range(0,len(br_kartice)-4):
+    skrivena.append(znak)
+    i=i+1
+print(skrivena)
+
+pod_lista=brojevi[-4:] 
+print(pod_lista)
+
+br_skriveni=skrivena + pod_lista
+print(br_skriveni)
+
+for broj in br_skriveni:
+    print(broj, end='')
+
+
+
