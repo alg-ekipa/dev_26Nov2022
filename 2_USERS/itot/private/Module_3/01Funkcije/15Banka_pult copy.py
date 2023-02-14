791183f04c4390b48bde8cf489f4c13bc5b55457
itot <itot@itot.tot>
2023-02-14 15:36:49 +0100

    itot

---
+++ 2_USERS/itot/private/Module_3/01Funkcije/15Banka_pult copy.py
@@ -0,0 +1,23 @@
+
+
+iznos = input()
+
+
+if iznos.isdecimal() == False:
+    print('false dec')
+
+if iznos.isdecimal() != False:
+    print('True dec')
+
+if iznos.isnumeric() != False:
+    print('True num')
+
+if iznos.isdecimal() == False:
+    print('false num')
+
+if iznos.is  () != False:
+    print('True num')
+
+if iznos.isdecimal() == False:
+    print('false num')
+
