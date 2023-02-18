ace0638f19e8dad81e71b03765bd65df6a19efc9
ivap <iva@piva.com>
2023-02-18 11:37:28 +0100

    novo

--- 2_USERS/icizm/private/Module_2/funkcije/IdentitiyManagement.py
+++ 2_USERS/icizm/private/Module_2/funkcije/IdentitiyManagement.py
@@ -11,6 +11,7 @@ def logiranje(uneseni_username):
         uneseni_password = input('Unesi lozinku: ')
         if uneseni_password == korisnici[uneseni_username][2]:
             print(f'Dobrodošli, {korisnici[uneseni_username][0]} {korisnici[uneseni_username][1]}')
+            jesamliadmin()
         else: 
             print('Pogrešna lozinka!')
     else: 
@@ -26,22 +27,28 @@ def dodavanje_korisnika():
     lozinka = input('Unesite lozinku: ')
     korisnici[k_ime] = [ime, prezime, lozinka]
     
+    
 def brisanje(): 
     '''Funkcija za brisanje korisnika '''
     clan_brisi = input('Unesi username korisnika kojeg želite obrisati: ')
     clan_brisi = korisnici.pop(clan_brisi)
     print(korisnici)
 
-#def izbornik(): 
-while True:
+def izbornik(): 
+#while True:
     odabir = input('Dobar dan! Odaberite jednu od sljedećih opcija: login | registracija  ')
     if odabir == 'login': 
         uneseni_username = input('Unesite svoje korisničko ime: ')
         logiranje(uneseni_username)
     if odabir == 'registracija': 
         dodavanje_korisnika()
-    else: 
-        break
+        print('Želite li se ulogirati? da | ne')
+        odabir_login = input()
+        if odabir_login == 'da':
+            uneseni_username = input('Unesite svoje korisničko ime: ')
+            logiranje(uneseni_username)
+
+    
 
 def pregled_korisnici(): 
     '''Funkcija koja ispisuje sve korisnike (ime i prezime) iz baze/riječnika'''
@@ -49,7 +56,7 @@ def pregled_korisnici():
         print({v[0]}, {v[1]})
 
 def admin_izbornik():
-    print(f'Odaberite broj administratorske opcije: \n\t 1) dodavanje  \n\t 2) brisanje \n\t 3)pregled \n\t 4) odjava')
+    print(f'Odaberite broj administratorske opcije: \n\t 1) dodavanje  \n\t 2) brisanje \n\t 3) pregled \n\t 4) odjava')
     odabir_admin = input()
     if odabir_admin == '1': 
         dodavanje_korisnika()
@@ -58,23 +65,33 @@ def admin_izbornik():
     if odabir_admin == '3': 
         pregled_korisnici()
     if odabir_admin == '4': 
-        
+        izbornik()
     
+def korisnik_izbornik(): 
+    print(f'Koje opcije želite koristiti: \n\t 1) pregled svojih podataka \n\t 2) odjava ')
+    odabir_korisnik = input()
+    if odabir_korisnik == '1': 
 
+'''def dohvati_admin_pswd():
+    for k, v in korisnici.items():
+        if k == 'admin':
+            return v[2]'''
 
+def dohvati_podatke_korisnika(): 
+    for k, v in korisnici.items(): 
+        if k == uneseni_username: 
+            return print
 def jesamliadmin(): 
     for k, v in korisnici.items(): 
         if k == 'admin':
             admin_izbornik()
+        else: 
+            korisnik_izbornik()
             
 
 
-zadatak = input(f'Dobro došli u program za konverziju mjernih jedinica! \nOdaberite konverziju: \n\t 1) udaljenost \n\t 2) temperatura \n\t 3) težina \n\t 4) volumen \n\t 5) snaga \n ')
-    if zadatak == '1':
 
-logiranje(uneseni_username)
-dodavanje_korisnika()
-#izbornik()
+izbornik()
 
 '''while 1:
     odg = input ('Želite li se prijaviti? da/ne ')
