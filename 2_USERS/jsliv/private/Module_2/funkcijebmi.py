1ea50baa981ce38578a33fedb21123589aca7d2c
July <july@july.com>
2023-02-20 21:26:14 +0100

    #save

--- 2_USERS/jsliv/private/Module_2/funkcijebmi.py
+++ 2_USERS/jsliv/private/Module_2/funkcijebmi.py
@@ -1,17 +1,17 @@
 name1 = "YK"
-height = 2
-weight = 90
+height1 = 2
+weight1 = 90
 
 name2 = "YK's sister"
-height = 1.8
-weight = 70
+height2 = 1.8
+weight2 = 70
 
 name3 = "YK's brother"
-height = 2.5
-weight = 160
+height3 = 2.5
+weight3 = 160
 
 def bmi_calculator(name,height, weight):
-    bmi = weight / (height **2)
+    bmi = round(weight / (height **2), 2)
     print("bmi: ")
     print(bmi)
     if bmi < 25:
@@ -19,9 +19,9 @@ def bmi_calculator(name,height, weight):
     else:
         return name + " is overweight"
 
-result1 = bmi_calculator(name1, height, weight)
-result2 = bmi_calculator(name2, height, weight)
-result3 = bmi_calculator(name3, height, weight)
+result1 = bmi_calculator(name1, height1, weight1)
+result2 = bmi_calculator(name2, height2, weight2)
+result3 = bmi_calculator(name3, height3, weight3)
 
 print(result1)
 print(result2)
