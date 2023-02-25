3fe5a93e72af0c03ea50f846917a3d747721f0f1
ImeE <ime@prezime.com>
2023-02-25 09:52:49 +0100

    xxx

---
+++ 2_USERS/zbene/private/Module_2/25.02.2023/01_parcijalni.py
@@ -0,0 +1,13 @@
+#Napravite program za rad na pultu u banci
+#Program za sada izvršava u konzoli funkcionalnosti:
+#1. Izbornik
+#2. Otvaranje računa tvrtke
+#3. Prikaz stanja računa
+#4. Prikaz prometa po računu
+#5. Polog novca na račun
+#6. Podizanje novca s računa
+#7. Izlaz iz programa (program se nakon svake akcije vrati na početni izbornik u kojem postoji opcija Izlaz)
+
+računi = {
+    '100100' : ['Banka X', '1524360.78']
+}
