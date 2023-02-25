f93e1018b94d406e312ca0c7a651d2f0c6c3b1a0
Tom <tom.tom.x>
2023-02-25 12:18:28 +0100

    Tom: course samples

---
+++ 2_USERS/tkoji/2. COURSES/PREDAVANJE_25_FEB_2023/Vesna_examples/basic_class.py
@@ -0,0 +1,21 @@
+
+class MyFirstCLass():
+    ''' Docstring '''
+    pass
+
+class Student():
+    name = 'Marko'
+    sname = 'Markovic'
+    oib = '0123456789'
+    addr = 'Hogwards 9-3/4'
+    grade = 4
+
+
+student1 = Student()
+print(f'Student: {student1.name} has grade {student1.grade}')
+
+student2 = Student()
+print(f'Student: {student2.name} has grade {student2.grade}')
+
+
+
