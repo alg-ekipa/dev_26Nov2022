8d1c74f881fd0856fe0efaec1b073da9e45b173c
July <july@july.com>
2023-02-26 20:48:25 +0100

    Save

--- 2_USERS/jsliv/private/Module_2/banka.py
+++ 2_USERS/jsliv/private/Module_2/banka.py
@@ -1,8 +1,8 @@
 import os
 
 korisnici = {
-    "00001" :["Mak d.o.o.", "2360000123456789", "50000"],
-    "00002": ["Sunset d.o.o.", "2360000987654321", "100000"], 
+    "00001" :["Mak d.o.o.", "236000012345678", "50000"],
+    "00002": ["Sunset d.o.o.", "236000098765432", "100000"], 
 }
 
 saldo = {
@@ -10,27 +10,53 @@ saldo = {
     "00002":["100000"]
 }
 
+administrator = {
+    "admin": ["Luka Lukic", "98765"]
+}
 #print(korisnici)
 
-def novi_korisnik():
-    naziv = input("Unesi naziv poduzeća: ")
-    racun = input("Unesi broj računa: ")
-    saldo = input("Unesi saldo računa: ")
-    sifra_korisnika = input("Unesi šifru korisnika, mora sadržavati 5 brojeva: ")
-    
-    while len(sifra_korisnika) > 5:
-        print("Šifra mora sadržavati 5 brojeva.")  
-        quit() 
-    
-    korisnici[sifra_korisnika] = naziv, racun, saldo
-    print(korisnici)
-novi_korisnik()
+def pregled_korisnika():
+    for korisnik in korisnici.values():
+        print(korisnik[0], korisnik[1], korisnik[2])
+pregled_korisnika()
+print()
 
+def logiranje(username):
+    if username in administrator.keys():
+        password = input("Unesi lozinku: ")
+        while password != administrator[password][1]:
+            print("Pogrešna lozinka, pokušajte ponovo.")
+            password = input("Unesi password: ")
+        else:
+            print(f"Dobrodosli {administrator[username][0]}!")
+    else:
+        print("Nepostojeće korisničko ime.")
+    print()
+
+def novi_korisnik():
+    sifra = input("Unesi sifru korisnika: ")
+    while len(sifra) != 5:
+        print("Sifra korisnika mora sadrzavati 5 znamenki.")
+        sifra = input("Ponovo unesi sifru korisnika: ")
+    else:
+        naziv = input("Unesi naziv poduzeća: ")
+        racun = input("Unesi broj računa: ")
+        while len(racun) != 15:
+            print("Broj računa mora sadržavati 15 znamenki.")
+            racun = input("Ponovo unesi broj računa: ")
+        saldo = input("Unesi saldo računa: ")
+        korisnici[sifra] = [naziv, racun, saldo]
+        print()
+        print("Korisnici u bazi: ")
+        pregled_korisnika()  
+novi_korisnik()
+print()
 
 def stanje_racuna():
     for kljuc, vrijednost in korisnici.items():
         print(korisnici[kljuc][2])
 stanje_racuna()
+print()
 
 def uplata():
     
