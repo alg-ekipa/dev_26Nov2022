d630563202aea0e60459e0fdca37d7ff4c505e8b
John Doe <johndoe@example.com>
2023-03-04 10:19:03 +0100

    xxx

--- 2_USERS/zbene/private/Module_3/04.03.2023/01klase5rjecnik.py
+++ 2_USERS/zbene/private/Module_3/04.03.2023/01klase5rjecnik.py
@@ -9,6 +9,19 @@ class Vozilo:
     def ispis (self):
         print (f'\nVrsta: {self.vrsta}\nProizvođač: {self.proizvođač}\nRegistracija: {self.registracija}\nGodina proizvodnje: {self.god_proizvodnje}\nCijena: {self.cijena}')
 
+    def ispis_dostavna(self):
+        if self.vrsta == 'Dostavno vozilo':
+            self.ispis()
+            print (f'\nVrsta: {self.vrsta}\nRegistracija: {self.registracija}')
+    
+    def ispis_starija_od_2015(self): #2. zadatak#
+        if self.god_proizvodnje <2015:
+            self.ispis()
+            print (f'\nVrsta {self.vrsta}\nProizvođač: {self.proizvođač}\nRegistracija: {self.registracija}\nGodina proizvodnje: {self.god_proizvodnje}\nCijena: {self.cijena}')
+
+    def traži_regu (self, tražena_registracija): #3. zadatak#
+        if tražena_rega == self.registracija:
+            self.ispis()
 
 vozni_park = {
     1 : ['Kamion', 'Iveco', 'OS 001 ZZ', 2015, 45000.00],
@@ -30,7 +43,24 @@ for vrijednost in vozni_park.values():
     g = vrijednost [3]
     c = vrijednost [4]
     vozilo_objekt = Vozilo (v, p, r, g, c)
-    vozilo_objekt.ispis()
-    #lista_objekata_vozila.append(vozilo_objekt)
+    #vozilo_objekt.ispis()
+    lista_objekata_vozila.append(vozilo_objekt)
 
 #print(lista_objekata_vozila)
+
+#ZADATAK: ispisati sva dostavna vozila - metoda ispis_dostavno() piše se u klasu Vozilo
+
+for vozilo in lista_objekata_vozila:
+    vozilo.ispis_dostavna()
+
+for vozilo in lista_objekata_vozila: #2 zadatak#
+    vozilo.ispis_starija_od_2015()
+
+for vozilo in lista_objekata_vozila: #3. zadatak#
+    vozilo.traži_regu(tražena_rega)
+
+#TO DO:
+# 1. ispišite sva vozila starija od 2015.g.
+# 2. ispis vozila tražene registracije
+
+tražena_registracija = input ('Unesi podatak o registraciji: ')
