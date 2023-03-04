01fae516ad322893a457da48826f585d3ead3862
HP <>
2023-03-04 12:16:37 +0100

    klasa

---
+++ 2_USERS/hpac/private/Module_3_Programiranje_u_Pythonu/12_klase_korisnik_godine.py
@@ -0,0 +1,5 @@
+# ZADATAK: 
+# Kreirati klasu korisnik (svojstva po izboru), ali mora imati datum rođenja. Ime i prezme unosi se kao jedna varijabla, koju zatim razdvojite u dvije
+# u klasi definirati metodu kojem se računa starost korinsika na način da se oduzme danjašnji datum od datuma njegovog rođenja
+# koristiti modul datetime
+# Kreirati par objekata i pozivati metodu i ispisati starost korisnika
