bcbada3ac4be02b4324d0639611cfbbc51bd3969
HP <>
2023-03-04 13:45:09 +0100

    klasa

--- 2_USERS/hpac/private/Module_3_Programiranje_u_Pythonu/12_klase_korisnik_godine.py
+++ 2_USERS/hpac/private/Module_3_Programiranje_u_Pythonu/12_klase_korisnik_godine.py
@@ -3,3 +3,22 @@
 # u klasi definirati metodu kojem se računa starost korinsika na način da se oduzme danjašnji datum od datuma njegovog rođenja
 # koristiti modul datetime
 # Kreirati par objekata i pozivati metodu i ispisati starost korisnika
+
+import datetime
+
+class Korisnik:
+    def __init__(self, ime_i_prezime, datum_rodenja, mjesto_rodenja):
+        self.ime_i_prezime = ime_i_prezime
+        self.datum_rodenja = datum_rodenja
+        self.mjesto_rodenja = mjesto_rodenja
+
+    def dobiti_ime(self):
+        return self.ime_i_prezime.split[0]
+    
+    def dobiti_prezime(self):
+        return self.ime_i_prezime.split[1]
+    
+korisnik1 = Korisnik('Josip Matić','','Osijek')
+korisnik2 = Korisnik('Ivana Ivić','','Gračac')
+
+print(datetime.date.today())
