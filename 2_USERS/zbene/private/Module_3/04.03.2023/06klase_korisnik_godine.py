56b1b8901572b63a2881e3f9e9a1647950d0c8ec
John Doe <johndoe@example.com>
2023-03-04 12:15:37 +0100

    xxx

---
+++ 2_USERS/zbene/private/Module_3/04.03.2023/06klase_korisnik_godine.py
@@ -0,0 +1,6 @@
+#ZADATAK
+#kreirati klasu Korisnik, svojstva po izboru, no mora imati datum rođenja i ime i prezime unosi se kao jedna varijabla koju zatim razdvojite u dvije
+#u klasi definirati metodu kojom se računa starost korisnika na način da se oduzme današnji datum od njegovog datuma rođenja
+#koristiti modul datetime
+#kreirati par objekata i pozivati metodu i ispisati starost korisnika/objekata
+
