8634b195c46af466311dbdcb99af194b7eed7115
SAn <san@san.com>
2023-03-18 09:38:23 +0100

    San

---
+++ 2_USERS/smsalo/private/Module_3/01_Klase/klasa_unutar_klase/Racuni_klase_unutar/Racuni_klase_unutar/kl_unutar_rac_main.py
@@ -0,0 +1,31 @@
+from kl_unutar_racuni import Racun
+
+# Unos više računa
+
+lista_objekata_racuna = []
+broj = 1
+datum = '1.1.2001.'
+
+
+while True:
+    r = Racun(broj, datum, '', 1)
+    lista_stavki = r.unesi_stavke()
+    ukupna_cijena = 0
+    print(r.broj, r.datum)
+    print('--------------------------------------')
+    broj+=1
+    for stavka in lista_stavki:
+        r = Racun(broj, datum, stavka.proizvod, stavka.cijena)
+        ukupna_cijena += r.stavka.cijena
+        r.ispisi()
+    print('__________________________________________')
+    print(ukupna_cijena)
+
+    
+
+    pitanje = input('Novi račun?  da / ne   ')
+    if pitanje == 'ne':
+        break
+
+
+
