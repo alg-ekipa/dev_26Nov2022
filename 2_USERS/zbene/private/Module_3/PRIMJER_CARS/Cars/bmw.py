c538fe7e52b886a55c0082ac4dff9af2fd89f6c6
dick <cock>
2023-03-18 13:43:01 +0100

    xxx

---
+++ 2_USERS/zbene/private/Module_3/PRIMJER_CARS/Cars/bmw.py
@@ -0,0 +1,8 @@
+class Bmw:
+    def __init__ (self):
+        self.models = ['i8', 'x1', 'x5', 'x6']
+
+    def ispis (self):
+        print ('Bmw - models')
+        for model in self.models:
+            print (model, end='')
