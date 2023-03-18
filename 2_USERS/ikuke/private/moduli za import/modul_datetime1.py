e218c1fa1c5961a4ff3c5f947ffc25319093d27f
Your Name <you@example.com>
2023-03-18 13:53:14 +0100

    #4

--- 2_USERS/ikuke/private/moduli za import/modul_datetime1.py
+++ 2_USERS/ikuke/private/moduli za import/modul_datetime1.py
@@ -3,3 +3,22 @@ import datetime as dt
 danas=dt.date.today() # današnji datum u obliku YYYY.MM.DD
 
 print(danas)
+
+
+sada_vrijeme=dt.time(12,30,0) # vrijeme kreirano preko parametara koje prenosimo
+
+
+print (sada_vrijeme)
+
+a=dt.time(12,30,30,444445)
+
+print(a)
+
+dan=dt.date.weekday(danas) #(danas #vraća broj 0-6 -- 0 , ponedjeljak, 1 - utorak)
+print ("danas je", dan)
+
+import locale
+locale.setlocale(locale.LC_TIME, 'hr_HR')
+
+
+print(danas)
