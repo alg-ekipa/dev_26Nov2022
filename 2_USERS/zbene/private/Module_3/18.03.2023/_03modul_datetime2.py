52c1a628887ca688dd69de3d8e1c1b00f61e2566
dick <cock>
2023-03-18 10:38:56 +0100

    xxxx

--- 2_USERS/zbene/private/Module_3/18.03.2023/_03modul_datetime2.py
+++ 2_USERS/zbene/private/Module_3/18.03.2023/_03modul_datetime2.py
@@ -5,3 +5,7 @@ from dateutil import tz
 sadašnji_trenutak = dt.datetime.now()
 neki_datum = dt.datetime (2020, 10, 5, 12, 0, 0)
 
+print ('Sadašnji trenutak:', sadašnji_trenutak)
+print ('Neki datum:', neki_datum)
+
+print ('Razlika između ta dva datuma/trenutka: ', sadašnji_trenutak-neki_datum)
