e48727badeea327e7489ab20208da22bf714d8f1
dick <cock>
2023-03-18 10:34:37 +0100

    xxx

---
+++ 2_USERS/zbene/private/Module_3/18.03.2023/_03modul_datetime2.py
@@ -0,0 +1,7 @@
+import datetime as dt
+from dateutil.relativedelta import relativedelta
+from dateutil import tz
+
+sadašnji_trenutak = dt.datetime.now()
+neki_datum = dt.datetime (2020, 10, 5, 12, 0, 0)
+
