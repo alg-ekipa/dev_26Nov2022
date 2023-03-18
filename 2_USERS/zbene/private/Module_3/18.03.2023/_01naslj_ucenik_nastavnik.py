4c0e04565258ddeba4c2e5971970522a891d0298
dick <cock>
2023-03-18 09:24:48 +0100

    xxx

---
+++ 2_USERS/zbene/private/Module_3/18.03.2023/_01naslj_ucenik_nastavnik.py
@@ -0,0 +1,4 @@
+#Napraviti klasu Osoba (parent) koju će naslijediti klase Učenik i Nastavnik
+#Neka se unose nekoliko učenika i nastavnika preko tipkovnice, kreiraju kao objekti, a zatim spreme u neku listu
+#lista_učenici i lista_nastavnici
+
