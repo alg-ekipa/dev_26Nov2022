3171c4fce7c8a4d9ef5dbfa8619e578b2bdeb6ad
dick <cock>
2023-03-18 10:09:38 +0100

    xxx

---
+++ 2_USERS/zbene/private/Module_3/18.03.2023/_02klase_6-rječnik.py
@@ -0,0 +1,17 @@
+import datetime as dt
+
+danas = dt.date.today() #današnji datum u obliku YYYY-MM--DD
+print (danas)
+
+sada = dt.datetime.now() #sadašnji trenutak
+print (sada)
+
+sada_vrijeme = dt.time (12, 30, 11,555111) #vrijeme kreirano preko parametara koje prenosimo
+print (sada_vrijeme)
+
+dan = dt.datetime.weekday (danas) #vraća broj 0-6, počinje 0:ponedjeljak
+print ('Danas je', dan)
+
+print (danas.strftime ('%A'))
+print (danas.strftime ('%d.%m.%Y.'))
+print ()
