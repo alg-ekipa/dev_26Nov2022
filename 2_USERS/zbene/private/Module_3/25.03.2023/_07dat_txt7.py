32f932f1b0c251ed960bea87cff1533915b323b4
krkan <krkan@gmail.com>
2023-03-25 12:32:10 +0100

    xxx

--- 2_USERS/zbene/private/Module_3/25.03.2023/_07dat_txt7.py
+++ 2_USERS/zbene/private/Module_3/25.03.2023/_07dat_txt7.py
@@ -7,35 +7,19 @@ stolovi_rjecnik = {
     '1773' : ['stol Lucas', 940.00, True, '120x90x80', 'crna', 'drvo']
 }
 
-counter = 1
-
-while stol in stolovi_rjecnik:
-    for k,v in stolovi_rjecnik:
-           materijal = v[6]
-
-    if materijal == 'drvo':
-        print()
+for k,v in stolovi_rjecnik.items():
+    šifra = k
+    ime = v[0]
+    cijena = [1]
+    raspoloživost = v[2]
+    boja = v[3]
+    materijal = v[4]
+    stanje = v[5]
 
     try:
-        file_writer = open('D:/python 26.11. grupa/dev_26Nov2022-1/2_USERS/zbene/private/Module_3/25.03.2023/adresar3.txt', 'a')
-        redak = f'{counter};{v[6]}\n'
-        file_writer.write(redak)
-        counter +=1
-
-    for red in file_reader:
-            dijelovi_retka = red.split(';')
-            #print (dijelovi_retka)
-            redni_broj = dijelovi_retka [0]
-            ime = dijelovi_retka [1]
-            prezime = dijelovi_retka [2]
-            mob = dijelovi_retka [3].rstrip
-            
-            contacts_object = Contact (redni_broj, ime, prezime, mob)
-            contacts_object.print_contact()
-
-
-    except Exception as e:
-        print (f'dogodila se pogreška {e}')
-
-    finally:
-        file_writer.close()
+        with open('D:/python 26.11. grupa/dev_26Nov2022-1/2_USERS/zbene/private/Module_3/25.03.2023/stolovi_rjecnik.txt', 'a', encoding='utf8') as file_writer:
+            redak = f'{šifra} {ime} {materijal}\n'
+            file_writer.write(redak)
+        
+    except Exception as ex:
+        print (f'dogodila se pogreška {ex}')
