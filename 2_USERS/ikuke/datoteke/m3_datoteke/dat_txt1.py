d8ad3dfab721a0c17b907030779d4bc55fc100a1
Your Name <you@example.com>
2023-03-25 10:34:27 +0100

    #p

---
+++ 2_USERS/ikuke/datoteke/m3_datoteke/dat_txt1.py
@@ -0,0 +1,51 @@
+
+import os
+os.system('cls')
+
+absolute_path = os.path.dirname(__file__)
+print(absolute_path)
+putanja=""
+
+i=len(absolute_path)
+for k in range (i):
+    slovo=absolute_path[k]
+    if slovo == "\\" :
+        putanja=putanja+"/"
+    else:
+        putanja=putanja+slovo
+
+print (absolute_path)
+print (putanja)
+
+
+
+#pisanje
+
+
+#kreiranje varijable u koju se pohranjuje konekcija prema datoteci
+# metoda (open) definira ime datoteke i aktivnost koju     ,dodavanje na kraj
+# #ako datoteka ne postoji, kreirat će se
+# 
+# 
+# 
+
+file1=putanja+"datoteka.txt"
+
+#aktivnost nad datotekom je write
+#file_writer = open(file1, "w") 
+
+
+#aktivnost nad datotekom je append
+file_writer = open(file1, "a") 
+
+
+ime = input ("Unesi ime i prezime:") 
+
+
+# upisivanje sadržaja u datoteku
+file_writer.write(ime)
+
+
+#zatvaranje konekcija i otpuštanje resursa
+
+file_writer.close()
