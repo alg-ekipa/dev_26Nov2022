3992c4c4cfca86d8f65485a87a2e2e988f368ec1
san <san@gmail.com>
2023-03-25 12:51:25 +0100

    San

---
+++ 2_USERS/smsalo/private/Module_3/04_json/dat_1.py
@@ -0,0 +1,30 @@
+import json
+
+user = {
+            "id": 1,
+            "firstName": "Petar Peric",
+            "lastName": "Petar Peric",
+            "username": "pperic",
+            "email": "pperic@email.adresa",
+            "address": {
+                "street": "Ilica 256",
+                "city": "Zagreb",
+                "zipcode": "10000"
+            },
+            "phone": "+385 1 8031 564",
+            "website": "web.adresa",
+            "company": {
+                "name": "Medvednica d.o.o.",
+                "catchPhrase": "Razvoj specijaliziranih Python aplikacija",
+                "bs": "Najbolja poslovna rjesenja"
+            }
+        }
+
+# UPISIVANJE u json datoteku
+
+try:
+    with open('user1.json', 'w') as file_writer:
+        json.dump(user, file_writer, indent=4)
+
+except Exception as e:
+    print(f'Pogreška {e}')
