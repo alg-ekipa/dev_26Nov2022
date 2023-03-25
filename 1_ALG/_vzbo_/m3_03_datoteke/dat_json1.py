cd6c0c4029f30998c4d56cb7b149e31d053cc594
VZ <vesna>
2023-03-25 12:51:31 +0100

    json

--- 1_ALG/_vzbo_/m3_03_datoteke/dat_json1.py
+++ 1_ALG/_vzbo_/m3_03_datoteke/dat_json1.py
@@ -1,5 +1,6 @@
 import json
 
+# podaci u obliku rječnika koje pohranjujemo u json datoteku
 user = {
             "id": 1,
             "firstName": "Petar Peric",
@@ -19,3 +20,13 @@ user = {
                 "bs": "Najbolja poslovna rjesenja"
             }
         }
+
+# upisivanje u json datoteku - metoda json.dump
+
+try:
+    with open('D:/Vesna/dev_26Nov2022/dev_26Nov2022/1_ALG/_vzbo_/m3_03_datoteke/user1.json', 'w') as file_writer:
+        json.dump(user, file_writer, indent=4)
+
+except Exception as e:
+    print(f'Pogreška: {e}')
+
---
+++ 1_ALG/_vzbo_/m3_03_datoteke/dat_json3_vjezba.py
@@ -0,0 +1,2 @@
+# Učitati datoteku json_example1 i izdvojiti punu adresu dostave paketa, ispisati jedno ispod drugog u txt datoteku adresa dostave
+
