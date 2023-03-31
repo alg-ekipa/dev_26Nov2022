ce4432707061264d5764d160cb47b000b6886cb0
itot <itot@itot.tot>
2023-03-31 23:00:08 +0200

    itot

--- 2_USERS/itot/private/Module_3/08Planer/glavni_planer.py
+++ 2_USERS/itot/private/Module_3/08Planer/glavni_planer.py
@@ -8,17 +8,17 @@ class Dogadaj:
 
 
 planer_it = {
-    '010123' : [('07:17', 'Test 0101230 7:17'), ('22:17', 'Test 0101230 22:17')],
-    '170423' : [],
-    '161123' : [],
+    '01.01.23' : [('07:17', 'Test 0101230 7:17'), ('22:17', 'Test 0101230 22:17')],
+    '17.04.23' : [('08:17', 'Test 17.04.23 8:17'), ('23:17', 'Test 17.04.23 23:17')],
+    '16.11.23' : [('10:17', 'Test 16.11.23 10:17 '), ('21:17', 'Test 16.11.23 21:17')]
    }
 
 
-print(planer_it)
+print(planer_it)  # TEST, izbrisati kasnije
 
 
 trazeno_vrijem ='22:17'
-trazeni_datum = '010123'
+trazeni_datum = '01.01.23'
 
 
 #ispis opisa po traženom vremenu i datumu
@@ -31,4 +31,38 @@ def ispis_opisa_datum_vrijeme(trazeni_datum,trazeno_vrijem):
             return y
 
 print(ispis_opisa_datum_vrijeme(trazeni_datum,trazeno_vrijem))
+'''
 
+unesni_datum = input('Upišite datum termina: dd.mm.gggg')
+if unesni_datum in planer_it.keys(): 
+    uneseno_vrijeme = input('Upišite vrijeme termina: hh:mm')
+    unesen_opis = input(f'Upišite opis termina u {uneseno_vrijeme}: ')
+    planer_it[unesni_datum].append((uneseno_vrijeme,unesen_opis))
+
+print(planer_it)
+
+'''
+
+import os, sys
+test_planer = 'im'
+
+# proba da se napravi novi rijecnik, ako ne postoji
+try:
+    if unesni_datum in planer_im.keys():
+        print('Planer postoji')
+except NameError:
+    print('Still working')
+    novi_planer = {}
+    print(novi_planer)
+    
+   # novi_planer = 'planer_' + test_planer
+
+
+
+
+    
+
+
+
+# renaming directory ''tutorialsdir"
+os.rename(f'{novi_planer}','planer_ {test_planer}')
