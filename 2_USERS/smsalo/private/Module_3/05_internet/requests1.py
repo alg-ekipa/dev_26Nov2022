e9cb09dc40c4c4827f5117bd7f21a1e53c1aac3f
San <san@san.com>
2023-04-01 11:45:08 +0200

    San

--- 2_USERS/smsalo/private/Module_3/05_internet/requests1.py
+++ 2_USERS/smsalo/private/Module_3/05_internet/requests1.py
@@ -1,3 +1,11 @@
-import requestes
+import requests
 
-url = ''
+url = 'https://algebra.hr'
+
+resp=requests.get(url)
+
+print(resp)             #
+print(resp.status_code) #kod odgovara na request (200,400,404,...)
+print(resp.content)     #
+print(resp.headers)
+print(resp.text)
