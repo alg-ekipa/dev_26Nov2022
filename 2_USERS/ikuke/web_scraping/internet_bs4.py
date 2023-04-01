3a678035fdd555a08a3053df1128e3246702f64b
Your Name <you@example.com>
2023-04-01 14:27:37 +0200

    7

--- 2_USERS/ikuke/web_scraping/internet_bs4.py
+++ 2_USERS/ikuke/web_scraping/internet_bs4.py
@@ -11,17 +11,69 @@ stranica_soup=BeautifulSoup(stranica)
 print(stranica_soup.prettify())
 
 populacija= stranica_soup.select('.country-population')
+imena_zemalja=stranica_soup.select('.country-name')
 
+
+
+
+
+
+
+
+
+ukupan_broj=0
 
 lista_populacija = []
 for broj in populacija:
+    ukupan_broj+=int(broj.text)
     lista_populacija.append(int(broj.text))
 
-print(lista_populacija)
+
+lista_zemalja = []
+for ime in imena_zemalja:
+    zemlja=ime.text
+    zemlja=zemlja.replace('\n','')
+    print(zemlja)
+    lista_zemalja.append(ime.text)
 
 
 
 
+
+
+
+rjecnik_iz_liste2 = {}
+
+lista_kljuceva = [1, 2, 3, 4, 5]
+lista_vrijednosti = ['python', 'c++', 'java', 'perl', 'asp.net']
+
+for i in range (len(lista_kljuceva)):
+    kljuc=lista_kljuceva[i]
+    print ('ključ---------------->', kljuc)
+    vrijednost=input ('Unesi vrijednost-->')
+
+    rjecnik_iz_liste2[kljuc]=vrijednost
+
+
+for k, v in rjecnik_iz_liste.items():
+    print(f'{k} : {v}')
+
+
+for k, v in rjecnik_iz_liste2.items():
+    print(f'{k} : {v}')
+
+
+
+
+
+
+
+#print(lista_populacija)
+print('ukupan broj ljudi je: ', ukupan_broj)
+
+print('ukupan broj ljudi je: ', ukupan_broj//1000000000, (ukupan_broj%1000000000)//1000000, ((ukupan_broj%1000000000)//1000)%1000, ukupan_broj%1000)
+
+#print(lista_zemalja)
 #TO DO
 
 #izračunati ukupnu populaciju i površinu svih država svijeta
