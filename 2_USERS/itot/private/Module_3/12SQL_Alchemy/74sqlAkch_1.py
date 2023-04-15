be5c5af11f9c351edd78362fcc6a8ee52197c0bb
itot <test@test.hr>
2023-04-15 14:13:37 +0200

    itot

---
+++ 2_USERS/itot/private/Module_3/12SQL_Alchemy/74sqlAkch_1.py
@@ -0,0 +1,19 @@
+# ako nedostaje SQLAlchemy:  pip install SQLAlchemy
+
+import sqlalchemy as sa
+
+engine = sa.create_engine('sqlite:///C:/Git/dev_26Nov2022/2_USERS/itot/private/Module_3/12SQL_Alchemy/skola.db')
+meta = sa.MetaData()
+
+ucenici = sa.Table(
+    'ucenici', meta, 
+    sa.Column('id', sa.Integer, primary_key = True),
+    sa.Column('ime', sa.String),
+    sa.Column('prezime', sa.String),
+)
+
+meta.create_all(engine)
+
+
+
+
