5ff534bc4de63dcd5baa8abfe6c007e948e661de
Piva <nekimail@piva.com>
2023-04-15 11:50:19 +0200

    stol

---
+++ 2_USERS/icizm/private/Module_3/SQLite/baza_sqlite_repo.py
@@ -0,0 +1,21 @@
+import sqlite3
+from SQLite_repo1 import *
+
+database_name = 'Proizvodi.db'
+
+stol_insert = ('Lucija', '50x70x100', 'bijela')
+
+query_create = ''' CREATE TABLE IF NOT EXISTS Stolovi
+                    (
+                    id INTEGER PRIMARY KEY,
+                    naziv VARCHAR(30) NOT NULL,
+                    dimenzije VARCHAR(20),
+                    boja VARCHAR(20)
+                    ); '''
+
+db_connection = create_connection(database_name) # poziv funkcije iz SQLite_repo.py module - objedinjenesu funkcije koje smo do sad koristili
+
+with db_connection: 
+    create_table(db_connection, query_create)  # poziv funkcije kreiranja tablice
+
+    create_stol(db_connection, stol_insert)
