7e464f8c05ace49587a139b978659b51035d04d3
Yame <yame@domain.example>
2023-04-22 13:29:34 +0200

    xxx

--- 2_USERS/zbene/private/Module_3/22.04.2023/gui_5.py
+++ 2_USERS/zbene/private/Module_3/22.04.2023/gui_5.py
@@ -9,6 +9,7 @@ for i in range (3):
     root.rowconfigure(i, weight=1, minsize=50)
 
     for j in range(3):
-        label = Label (root, text='L')
+        label = Label (root, text=f'{i},{j}')
+        label.grid(row=i, column=j, pady =15, padx=15)
 
 root.mainloop()
