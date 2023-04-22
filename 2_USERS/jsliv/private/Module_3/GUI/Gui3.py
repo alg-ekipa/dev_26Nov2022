72d57f2a30ac3216874959683865119dc07e1873
js <js@mail.com>
2023-04-22 14:02:58 +0200

    Save

--- 2_USERS/jsliv/private/Module_3/GUI/Gui3.py
+++ 2_USERS/jsliv/private/Module_3/GUI/Gui3.py
@@ -4,9 +4,20 @@ root = Tk()
 
 root.geometry("600x400")
 
-img_py = PhotoImage(file=)
+img_py = PhotoImage(file="C:/Users/JulijanaS/Desktop/VisualStudioCode/dev_26Nov2022-1/2_USERS/jsliv/private/Module_3/GUI/python.gif")
 
 gumb1 = Button(root, text = "Button")
 gumb2 = Button(root, text="Klikni Button2")
 gumb3 = Button(root, text="Prikazi sliku",
+                image=img_py,
+                command=LEFT,
+                relief=GROOVE,  # ako stavimo RAISED tada će gumb biti ispupčen
+                state=DISABLED   #ako stavimo umjesto DISABLED NORMAL onda će se gumb moći stisnuti
                 )
+
+gumb1.pack(pady=10)
+gumb2.pack(pady=10)
+
+gumb3.pack()
+
+root.mainloop()
