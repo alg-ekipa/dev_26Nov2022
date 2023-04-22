385db0aa9e7eff89f640e0f491367bd9962afe81
Piva <nekimail@piva.com>
2023-04-22 11:39:57 +0200

    cmt

---
+++ 2_USERS/icizm/private/Module_3/04-22_FOTODATOTEKE/photo_8_opencv.py
@@ -0,0 +1,4 @@
+# koriste se XML modeli 
+# prepoznavanje lica - radi se samo na crno-bijeloj fotografiji - koncertira se prvo fotka u CB pa se nakon toga koristi XML
+# pogledati u njenom folderu Library za to 
+
