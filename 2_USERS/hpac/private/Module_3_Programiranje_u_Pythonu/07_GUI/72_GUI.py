aea7d56f86e28d36590396a9edad863794df20a2
hp <h@p>
2023-04-22 13:59:01 +0200

    H

--- 2_USERS/hpac/private/Module_3_Programiranje_u_Pythonu/07_GUI/72_GUI.py
+++ 2_USERS/hpac/private/Module_3_Programiranje_u_Pythonu/07_GUI/72_GUI.py
@@ -5,7 +5,7 @@ root.geometry('600x400')
 root.title('ALG-ekipa')
 
 frame1 = Frame(root, bg='light grey', border=3, width=200, height=100)
-frame2 = Frame(root, bg='magenta', border=5, width=200, height=75)
+frame2 = Frame(root, bg='magenta', border=5, width=200, height=100)
 
 label1= Label(frame1, text='Na prvom okviru')
 label2 =Label(frame1, text='Na frame1, drugi tekst')
