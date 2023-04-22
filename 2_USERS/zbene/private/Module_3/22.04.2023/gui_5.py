bd0a4fb5571eb53f05942d13791d802c5958a87c
Yame <yame@domain.example>
2023-04-22 13:27:49 +0200

    xxx

---
+++ 2_USERS/zbene/private/Module_3/22.04.2023/gui_5.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+root.geometry ('600x400')
+root.title ('Alg-ekipa')
+
+for i in range (3):
+    root.columnconfigure(i, weight=1, minsize=50)
+    root.rowconfigure(i, weight=1, minsize=50)
+
+    for j in range(3):
+        label = Label (root, text='L')
+
+root.mainloop()
