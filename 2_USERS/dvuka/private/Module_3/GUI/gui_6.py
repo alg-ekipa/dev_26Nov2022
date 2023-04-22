52e664b5480d263c08ad4db7d9f810c1aeee68d9
DV <DV>
2023-04-22 13:52:45 +0200

    DV

--- 2_USERS/dvuka/private/Module_3/GUI/gui_6.py
+++ 2_USERS/dvuka/private/Module_3/GUI/gui_6.py
@@ -5,7 +5,7 @@ root.geometry('600x400')
 root.title('Alg-ekipa')
 
 frame1=Frame(root, bg='blue', bd=10, width=200, height=100)
-frame2=Frame(root, bg='red', bd=10, width=200, height=100)
+frame2=Frame(root, bg='blue', bd=10, width=200, height=100)
 
 label1=Label(frame1, text='Na frame1_prva', bg='blue', fg='white')
 label2=Label(frame1, text='Na frame1_druga', bg='blue', fg='white')
