a192d66a2c862a487fa9e68ef9b66441d9e42376
TOm <TOm>
2023-05-06 14:05:59 +0200

    Closing frame

--- 2_USERS/tkoji/2. COURSES/m3_07_GUI/parc.py
+++ 2_USERS/tkoji/2. COURSES/m3_07_GUI/parc.py
@@ -4,6 +4,7 @@ from tkinter import messagebox
 root = tk.Tk()
 root.title('Alg ekipa')
 
+closer = tk.BooleanVar()
 
 def ring():
     messagebox.showinfo(title="Greetings!", message="Ringing...")
@@ -13,12 +14,18 @@ def unlock():
     label_locker_settings = tk.LabelFrame(frame_settings, text='Unlock the safe here.')
     label_locker_settings.columnconfigure((0,1,2), weight=1, minsize=90)
     label_locker_settings.grid(row=2, column=0, columnspan=3, padx=30, pady=30,ipadx=20, ipady=10)
-    ch_box_locker = tk.Checkbutton(label_locker_settings, text='Click to unlock.', command=)
+    #ch_box_locker = tk.Checkbutton(label_locker_settings, text='Click to unlock.')
+    ch_box_locker = tk.Checkbutton(label_locker_settings, text='Click to unlock.', variable=closer)
     ch_box_locker.grid(row=0, column=2)
-
-def unlock_safe():
-    close_button = 
-
+    
+    if closer == True:
+        close_locker()
+    else:
+        pass
+    
+    def close_locker():
+        label_locker_settings.destroy()
+        
 frame_settings = tk.Frame(root)
 frame_settings.grid(row = 0, column=0, rowspan=3, columnspan=1)
 
