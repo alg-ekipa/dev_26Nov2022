0e9283dabdbea5b99a8d9bb83f69bb2f3ff183c6
TOm <TOm>
2023-05-06 13:54:44 +0200

    thinker

---
+++ 2_USERS/tkoji/2. COURSES/m3_07_GUI/parc.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+from tkinter import messagebox
+
+root = tk.Tk()
+root.title('Alg ekipa')
+
+
+def ring():
+    messagebox.showinfo(title="Greetings!", message="Ringing...")
+
+def unlock():
+    messagebox.showinfo(title="Locker", message="Please unlock the safe...")
+    label_locker_settings = tk.LabelFrame(frame_settings, text='Unlock the safe here.')
+    label_locker_settings.columnconfigure((0,1,2), weight=1, minsize=90)
+    label_locker_settings.grid(row=2, column=0, columnspan=3, padx=30, pady=30,ipadx=20, ipady=10)
+    ch_box_locker = tk.Checkbutton(label_locker_settings, text='Click to unlock.', command=)
+    ch_box_locker.grid(row=0, column=2)
+
+def unlock_safe():
+    close_button = 
+
+frame_settings = tk.Frame(root)
+frame_settings.grid(row = 0, column=0, rowspan=3, columnspan=1)
+
+label_frame_settings = tk.LabelFrame(frame_settings, text='Settings')
+label_frame_settings.columnconfigure((0,1,2), weight=1, minsize=90)
+label_frame_settings.grid(row=0, column=0, columnspan=3, padx=30, pady=30,ipadx=20, ipady=10)
+
+rb_show_password = tk.Button(label_frame_settings, text='Ring', command=ring)
+rb_show_password.grid(row=1, column=0)
+
+rb_hide_password = tk.Button(label_frame_settings, text='Unlock', command=unlock)
+rb_hide_password.grid(row=1, column=2)
+
+
+root.mainloop()
+
