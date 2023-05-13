8675ee3a7b848723f1221464b7404c035a02f089
John Doe <johndoe@example.com>
2023-05-13 09:38:37 +0200

    xxx

---
+++ 2_USERS/zbene/private/Module_4/13.05.2023/gui_unos_slova.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title('Algebra - PyDev: Unos slova')
+root.geometry ('600x400')
+
+label_naslov = tk.Label(root, text='Key Event', font=('Segoe UI',18))
+label_naslov.grid(row=0, column=0)
+
+
+root.mainloop()
