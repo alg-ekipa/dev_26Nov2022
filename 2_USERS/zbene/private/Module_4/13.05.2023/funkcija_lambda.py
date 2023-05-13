39d1921a0848f0534cea989fdf714d4245f7f8da
John Doe <johndoe@example.com>
2023-05-13 10:10:47 +0200

    xxx

--- 2_USERS/zbene/private/Module_4/13.05.2023/funkcija_lambda.py
+++ 2_USERS/zbene/private/Module_4/13.05.2023/funkcija_lambda.py
@@ -5,3 +5,12 @@ def povecaj_za_10(a):
     return a
 
 print(povecaj_za_10(18))
+
+#Lambda funkcija - spoj varijable i funkcije
+#'anonimna' funkcija - nema imena, nema return, ono što vraća odmah ide u varijablu
+
+x = lambda a: a+10
+print(x(18))
+
+y = lambda a,b: a*b
+print(y(5,7))
