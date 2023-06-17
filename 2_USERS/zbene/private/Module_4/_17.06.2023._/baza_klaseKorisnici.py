aab1b89de624a0b462f68f021257b61d85261ac7
John Doe <johndoe@example.com>
2023-06-17 12:25:58 +0200

    xxx

--- 2_USERS/zbene/private/Module_4/_17.06.2023._/baza_klaseKorisnici.py
+++ 2_USERS/zbene/private/Module_4/_17.06.2023._/baza_klaseKorisnici.py
@@ -13,19 +13,20 @@ class BazaKorisnici:
                                 ime TEXT,
                                 prezime TEXT,
                                 OIB TEXT,
-                                slika BLOB);''')            #SLIKA OBLIKA BLOB U BAZI    
-
-    def procitaj_sliku(self, slika_jpg):
-        slika_blob=open(slika_jpg, "rb").read()     
-        return slika_blob             
-    
-    def dodaj_korisnika(self, ime, prezime, OIB, slika):
+                                );''')            
+        
+    def dodaj_korisnika (self, ime, prezime, OIB):
         query_dodaj='''INSERT INTO Korisnici (ime, prezime, OIB, slika)
                         VALUES (?,?,?,?)'''
-        slika=self.procitaj_sliku(slika)
-        values=(ime, prezime, OIB, slika)
+        values=(ime, prezime, OIB)
         self.cursor.execute(query_dodaj, values)
         self.connection.commit()
+
+    def obriši_korisnika (self, OIB):
+        pass
+
+    def ažuriraj_korisnika (self, OIB):
+        pass
 
     def zatvori_konekciju(self):
         self.connection.close()
@@ -33,7 +34,4 @@ class BazaKorisnici:
 
 baza_objekt= BazaKorisnici('Korisnici_baza.db')     #ovim dijelom pozvat će se samo dio __init__ tj. otvorit će se konekcija
 baza_objekt.kreiraj_tablicu()
-baza_objekt.dodaj_korisnika('Jure', 'Juric', '123456789','C:/Users/Korisnik/Documents/San/dev_26Nov2022/2_USERS/smsalo/private\Module_3/11_klasa_nakon GUI/slika.jpg')
 baza_objekt.zatvori_konekciju()
-
-
