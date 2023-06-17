20d4c4dde58552ff8be60ec5a111319d372a0038
John Doe <johndoe@example.com>
2023-06-17 12:27:47 +0200

    xxx

--- 2_USERS/zbene/private/Module_4/_17.06.2023._/baza_klaseKorisnici.py
+++ 2_USERS/zbene/private/Module_4/_17.06.2023._/baza_klaseKorisnici.py
@@ -5,33 +5,32 @@ class BazaKorisnici:
         self.connection = sqlite3.connect(db_file)
         self.cursor = self.connection.cursor()
 
-    #metode za aktivan rad s bazom (kreiranje, pisanje, brisanje,...)
+    # Metode za aktivan rad s bazom (kreiranje, pisanje, brisanje, ...)
 
     def kreiraj_tablicu(self):
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS Korisnici
                                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
                                 ime TEXT,
                                 prezime TEXT,
-                                OIB TEXT,
-                                );''')            
-        
-    def dodaj_korisnika (self, ime, prezime, OIB):
-        query_dodaj='''INSERT INTO Korisnici (ime, prezime, OIB, slika)
-                        VALUES (?,?,?,?)'''
-        values=(ime, prezime, OIB)
+                                OIB TEXT);''')
+
+    def dodaj_korisnika(self, ime, prezime, OIB):
+        query_dodaj = '''INSERT INTO Korisnici (ime, prezime, OIB)
+                        VALUES (?, ?, ?)'''
+        values = (ime, prezime, OIB)
         self.cursor.execute(query_dodaj, values)
         self.connection.commit()
 
-    def obriši_korisnika (self, OIB):
+    def obriši_korisnika(self, OIB):
         pass
 
-    def ažuriraj_korisnika (self, OIB):
+    def ažuriraj_korisnika(self, OIB):
         pass
 
     def zatvori_konekciju(self):
         self.connection.close()
 
 
-baza_objekt= BazaKorisnici('Korisnici_baza.db')     #ovim dijelom pozvat će se samo dio __init__ tj. otvorit će se konekcija
+baza_objekt = BazaKorisnici('Korisnici_baza.db')
 baza_objekt.kreiraj_tablicu()
 baza_objekt.zatvori_konekciju()
